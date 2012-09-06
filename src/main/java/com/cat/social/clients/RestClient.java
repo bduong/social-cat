3a60c2730e6f8d3f60e5390fd00a93a8479dcfef
Ben Duong <bduong@vmware.com>
2012-09-06 11:56:14 -0400

    Added shells for SM clients

---
+++ src/main/java/com/cat/social/clients/RestClient.java
@@ -0,0 +1,4 @@
+package com.cat.social.clients;
+
+public interface RestClient {
+}
---
+++ src/main/java/com/cat/social/clients/TwitterClient.java
@@ -0,0 +1,11 @@
+package com.cat.social.clients;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bduong
+ * Date: 9/1/12
+ * Time: 4:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TwitterClient {
+}
